ca70f763c992346edc4f9afd17e6872851e632e4
Erika Espinoza <evepinoza@gmail.com>
2023-08-11 12:57:11 -0400

    o

--- vue/src/components/TheHeader.vue
+++ vue/src/components/TheHeader.vue
@@ -3,8 +3,8 @@
       <h1 id="h1">Vine <img id="pic" src="../assets/logo.gif"> </h1>
     <b-card id="nav" title="Card Title" no-body>
     <b-card-header header-tag="nav">
-      <b-nav  card-header pills>
-        <b-nav-item id = "home" class = "active" active><router-link v-bind:to="{ name: 'home' }">Home</router-link></b-nav-item>
+      <b-nav card-header pills>
+        <b-nav-item id = "home" class = "link-dark active" active><router-link v-bind:to="{ name: 'home' }">Home</router-link></b-nav-item>
         <b-nav-item id = "home"><router-link v-bind:to=" { name: 'plant-search' } ">Search</router-link></b-nav-item>
         <b-nav-item id = "home"><router-link v-bind:to="{ name: 'login' }" v-if="$store.state.token == ''"
         >Login</router-link
