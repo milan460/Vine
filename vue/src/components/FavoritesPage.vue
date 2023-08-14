154abe49a6eee869a6783da26ded532ab8265ce0
Dedric Isaac <d.w.isaac123@gmail.com>
2023-08-14 12:08:36 -0400

    added seller form

--- vue/src/components/FavoritesPage.vue
+++ vue/src/components/FavoritesPage.vue
@@ -15,6 +15,7 @@
 
             <b-button href="#" @click="removeFromfavoritesDatabase(favoriteItem.favoriteId)" variant="primary">delete From Favorites</b-button>
             <b-button href="#" @click="updateOwned(favoriteItem.favoriteId)" variant="secondary">Own this Plant?</b-button>
+            <b-button href="#" v-if="favoriteItem.ownedPlant === true" @click="sendToSellerForm" variant="secondary">Sell this Plant?</b-button>
           </b-card>
     </div>
     
@@ -103,6 +104,10 @@ export default {
 
       return default_image;
     },
+
+    sendToSellerForm(){
+      this.$router.push()
+    }
     
      
   },
---
+++ vue/src/views/SellerFormView.vue
@@ -0,0 +1,16 @@
+<template>
+  <seller-form></seller-form>
+</template>
+
+<script>
+import SellerForm from '../components/SellerForm.vue'
+export default {
+components:{
+    SellerForm
+}
+}
+</script>
+
+<style>
+
+</style>
