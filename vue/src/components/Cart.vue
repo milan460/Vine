d2f31ea61e6bd8e03ce5a266bf2028ffe66ca093
Dedric Isaac <d.w.isaac123@gmail.com>
2023-08-14 23:49:49 -0400

    fleshed out the pages of the front end

--- vue/src/components/Cart.vue
+++ vue/src/components/Cart.vue
@@ -1,35 +1,72 @@
 <template>
   <div>
-      <button @click="routeToCheckoutPage">Checkout</button>
-      <b-sidebar id="sidebar-right" title="Sidebar" right shadow>
-                <div class="px-3 py-2">
-                  <p>
-                    Our Checkout Items
-                  </p>
-                  <div v-for="item in this.$store.state.cartArray" v-bind:key="item.favoritesId">
-                    {{item}}
-                </div>
-                </div>
-              </b-sidebar>
+    
+    <b-sidebar id="sidebar-right" title="Cart" right shadow>
+      <div class="cart-content">
+        <h3 class="cart-title">Your Cart</h3>
+        <b-card v-for="item in this.$store.state.cartArray" :key="item.favoriteId" class="cart-item">
+          <b-card-text>
+            <p class="item-name">{{ item.plantObj.common_name }}</p>
+            <img :src="item.plantObj.default_image.medium_url" alt="Plant image" class="item-image">
+            <p class="item-price">${{ item.price }}</p>
+            <p class="item-quantity">{{ item.stockQuantity }} in stock</p>
+          </b-card-text>
+        </b-card>
+        <b-button @click="routeToCheckoutPage" variant="primary" class="checkout-button">Checkout</b-button>
+      </div>
+    </b-sidebar>
   </div>
-  
 </template>
 
 <script>
 export default {
-    // data(){
-    //     return {
-    //         item: []
-    //     }
-    // },
-    methods:{
-        routeToCheckoutPage(){
-            this.$router.push({ name: 'checkout'})
-        }
-    }
-}
+  methods: {
+    routeToCheckoutPage() {
+      this.$router.push({ name: 'checkout' });
+    },
+  },
+};
 </script>
 
 <style>
+.cart-content {
+  padding: 20px;
+}
 
+.cart-title {
+  font-size: 20px;
+  margin-bottom: 10px;
+}
+
+.cart-item {
+  margin-bottom: 15px;
+  border: none;
+}
+
+.item-name {
+  font-size: 16px;
+  font-weight: bold;
+  margin-bottom: 5px;
+}
+
+.item-image {
+  width: 100%;
+  max-height: 150px;
+  object-fit: cover;
+  margin-bottom: 10px;
+}
+
+.item-price {
+  font-size: 16px;
+  margin-bottom: 5px;
+}
+
+.item-quantity {
+  font-size: 14px;
+  color: #888;
+}
+.checkout-button{
+   margin-top: auto;
+  align-self: flex-end;
+}
 </style>
--- vue/src/components/MarketPlacePage.vue
+++ vue/src/components/MarketPlacePage.vue
@@ -3,121 +3,164 @@
     <div>
       <cart></cart>
     </div>
-      <div id="listingCard" v-for="listingItem in SellerListings" v-bind:key="listingItem.favoritesId">
-
-        <!-- {{listingItem.plantObj.common_name}} -->
-          <b-card
-            :title="listingItem.plantObj.common_name"
-            v-bind:img-src="listingItem.plantObj.default_image.medium_url"
-            alt="Plant Image"
-            img-top
-            tag="article"
-            style="max-width: 20rem"
-            class="mb-2 flex1"
+    <div
+      id="listingCard"
+      v-for="listingItem in SellerListings"
+      v-bind:key="listingItem.favoritesId"
+    >
+      <!-- {{listingItem.plantObj.common_name}} -->
+      <!-- <b-card
+        :title="listingItem.plantObj.common_name"
+        v-bind:img-src="listingItem.plantObj.default_image.medium_url"
+        alt="Plant Image"
+        img-top
+        tag="article"
+        style="max-width: 20rem"
+        class="mb-2 flex1"
+      >
+        <b-card-text>
+          <img
+            src="../assets/indoor-plants.png"
+            v-if="checkIndoorPlants === true"
+            :title="plant.indoor"
+          />
+          //indoor
+          <img
+            class="ratingStar"
+            src="../assets/drop.png"
+            v-bind:title="listingItem.plantObj.watering"
+          />
+          //watering
+          <ul
+            v-for="sunlight in listingItem.plantObj.sunlight"
+            v-bind:key="sunlight"
           >
-            <b-card-text>
-              <img src="../assets/indoor-plants.png" v-if="checkIndoorPlants === true" :title="plant.indoor"> //indoor
+            <ol>
               <img
-          class="ratingStar"
-          src="../assets/drop.png"
-          v-bind:title="listingItem.plantObj.watering"
-        /> //watering
-              <ul v-for="sunlight in listingItem.plantObj.sunlight" v-bind:key="sunlight">
-                <ol>
-                  <img
-          class="ratingStar"
-          src="../assets/sun.png"
-          v-bind:title="listingItem.plantObj.sunlight"
-        /> //sunlight
-                </ol>
+                class="ratingStar"
+                src="../assets/sun.png"
+                v-bind:title="listingItem.plantObj.sunlight"
+              />
+              //sunlight
+            </ol>
+          </ul>
+        </b-card-text>
 
-              </ul>
-            </b-card-text>
+        <b-button
+          href="#"
+          @click="addToCart(listingItem.favoritesId)"
+          variant="primary"
+          >Add To Cart</b-button
+        >
+      </b-card> -->
+      <!-- DIVIDING COMMENT -->
+  <b-card
+        :title="listingItem.plantObj.common_name"
+        v-bind:img-src="listingItem.plantObj.default_image.medium_url"
+        alt="Plant Image"
+        img-left
+        class="mb-3"
+      >
+        <b-card-text>
+          Seller Name: 
+          <strong>{{ listingItem.username }}</strong>
+           Plant Description:
+          <strong>{{ listingItem.description }}</strong>
+          Price:
+          <strong>${{listingItem.price}}</strong>
+           Stock:
+          <strong>{{listingItem.stockQuantity}}</strong>
 
-            <b-button href="#" @click="addToCart(listingItem.favoritesId)" variant="primary">Add To Cart</b-button>
-          </b-card>
-
-      </div>
+        </b-card-text>
+        <b-button
+          href="#"
+          @click="addToCart(listingItem.favoritesId)"
+          variant="primary"
+          >Add To Cart</b-button
+        >
+      </b-card>
+    </div>
   </div>
 </template>
 
 <script>
-import PlantData from '../services/PlantData.js'
-import SellerService from '../services/SellerService.js'
-import Cart from '../components/Cart.vue'
+import PlantData from "../services/PlantData.js";
+import SellerService from "../services/SellerService.js";
+import Cart from "../components/Cart.vue";
 export default {
-  data(){
-    return{
+  data() {
+    return {
       SellerListings: [
         {
-          username: '',
-          favoritesId: '',
-          plantId: '',
-          description: '',
-          price: '',
-          stockQuantity: '',
-          isAvailable: '',
-          plantObj: {}
+          username: "",
+          favoritesId: "",
+          plantId: "",
+          description: "",
+          price: "",
+          stockQuantity: "",
+          isAvailable: "",
+          plantObj: {},
+        },
+      ],
+    };
+  },
+  components: {
+    Cart,
+  },
+  methods: {
+    getAllListings() {
+      SellerService.getAllSellerListings().then((response) => {
+        if (response.status === 200) {
+          this.SellerListings = response.data.map((listingItem) => {
+            return {
+              username: listingItem.username,
+              favoritesId: listingItem.favoritesId,
+              plantId: listingItem.plantId,
+              description: listingItem.description,
+              price: listingItem.price,
+              stockQuantity: listingItem.stockQuantity,
+              isAvailable: listingItem.available,
+              plantObj: listingItem.plantObj,
+            };
+          });
+          this.getPlantData();
+          console.log("this is the seller listing");
+          console.log(this.SellerListings);
         }
-      ]
-    }
-  },
-  components:{
-    Cart
-  },
-  methods:{
-    getAllListings(){
-      SellerService.getAllSellerListings().then( (response) => {
-      if(response.status === 200){
-        this.SellerListings = response.data.map( (listingItem) => {
-          return {
-            username: listingItem.username,
-            favoritesId: listingItem.favoritesId,
-            plantId: listingItem.plantId,
-            description: listingItem.description,
-            price: listingItem.price,
-            stockQuantity: listingItem.stockQuantity,
-            isAvailable: listingItem.available,
-            plantObj: listingItem.plantObj
+      });
+    },
+    getPlantData() {
+      this.SellerListings.forEach((listingItem) => {
+        PlantData.getPlantDetails(listingItem.plantId).then((response) => {
+          if (response.status === 200) {
+            listingItem.plantObj = response.data;
           }
-        })
-        this.getPlantData()
-        console.log('this is the seller listing')
-        console.log(this.SellerListings)
-    
-    
+        });
+      });
+    },
+
+    addToCart(favoriteId) {
+      console.log(this.$store.state.currentFavoriteId);
+      console.log(favoriteId);
+      const selectedListing = this.SellerListings.find(
+        (listingItem) => listingItem.favoritesId === favoriteId
+      );
+      if (selectedListing.username === this.$store.state.user.username) {
+        alert("This is your own listing. Please select a different listing");
+      } else {
+        console.log("this is the add to cart item");
+        console.log(this.$store.state.user.username);
+        this.$store.commit("ADD_TO_CART_ARRAY", selectedListing);
+        console.log(this.$store.state.cartArray);
       }
-    })
     },
-    getPlantData(){
-      this.SellerListings.forEach(listingItem => {
-        PlantData.getPlantDetails(listingItem.plantId).then(response =>{
-          if(response.status === 200){
-            listingItem.plantObj = response.data
-          }
-        })
-      })
-    },
-
-    addToCart(favoriteId){
-      console.log(this.$store.state.currentFavoriteId)
-      console.log(favoriteId)
-      let listingItem = this.SellerListings.filter( (listingItem) => {
-        return listingItem.favoritesId === favoriteId
-      })
-      console.log('this is the add to cart item')
-      console.log(listingItem)
-      this.$store.commit('ADD_TO_CART_ARRAY', listingItem)
-      console.log(this.$store.state.cartArray)
-    }
   },
 
-  created(){
-    this.getAllListings()
-  }
-}
+  created() {
+    this.getAllListings();
+  },
+};
 </script>
 
 <style>
-
 </style>
